c5ad0440ad55046731ed9c6e7659803118b49e66
AlishbaGhori <126356642+AlishbaGhori@users.noreply.github.com>
2023-08-09 14:22:58 -0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+//////  ANSWER 1  //////
+
+alert("Error! Please enter a valid password");
+
+//////  ANSWER 2  ////////
+
+alert("Welcome to JS Land...\n" + "Happy Coding");
+
+/////  ANSWER 3  ////////////
+alert("Welcome to JS Land...");
+
+//////  ANSWER 4  ////////
+
+alert("Happy Coding! \n" + "prevent this page from creating additional dialogs.");
+//.........
+
+//////  ANSER 5  //////////////
+
+alert("Hello... I can run JS through my web browser's console");
+
+////////////////////
+
+
